c5959d64163ca4b17929025fc25a3668b316a492
Joshua Choe <choejd@usc.edu>
2023-11-07 18:18:38 -0800

    scrape player stats season by season from whoscored.com, parse scraped data, and insert/update player and club collection on mongodb

--- web-scraper.py
+++ web-scraper.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -9,13 +10,31 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
 from dotenv import load_dotenv
 import os
-
+import pprint
+import pymongo
 # Load environment variables from .env file
 load_dotenv()
 
 # Use environment variables
 CHROME_DRIVER_PATH = os.getenv('CHROME_DRIVER_PATH')
 USER_AGENT = os.getenv('USER_AGENT')
+MONGODB_CONNECTION_STRING = os.getenv('MONGODB_CONNECTION_STRING')
+
+
+league_abbreviation_to_name = {
+    'FL1': "Ligue 1",
+    'SLL': "LaLiga",
+    'UCL': "UEFA Champions League",
+    'UEL': "UEFA Europa League",
+    'WC': "FIFA World Cup",
+    'ICA': "Copa America",
+    'GB': "Bundesliga",
+    'UEC': "UEFA European Championship",
+    'EPL': "Premier League",
+    'ISA': "Serie A",
+    'UMLS': "Major League Soccer"
+}
+
 
 def parse_player_profile_text(player_profile_text):
     player_profile = {}
@@ -26,7 +45,6 @@ def parse_player_profile_text(player_profile_text):
     for line in lines:
         # Split the line into words
         words = line.split()
-    
         first_word = words[0]
         if first_word == "Name:":
             player_profile["name"] = " ".join(words[1:])
@@ -34,9 +52,9 @@ def parse_player_profile_text(player_profile_text):
             continue
         elif first_word == "Current":
             if len(words) == 2:
-                player_profile["current-team"] = "N/A"
+                player_profile["current-club"] = "N/A"
             else:
-                player_profile["current-team"] = " ".join(words[2:])
+                player_profile["current-club"] = " ".join(words[2:])
         elif first_word == "Shirt":
             player_profile["shirt-number"] = int(words[2])
         elif first_word == "Age:":
@@ -49,8 +67,52 @@ def parse_player_profile_text(player_profile_text):
             player_profile["positions"] = " ".join(words[1:])
             
     return player_profile
+    
+def parse_player_stats_text(player_stats_text, player_profile):
+    player_stats = []
+    season_and_club = True
+    season_stats = {}
+    
+    lines = player_stats_text.strip().splitlines()
+    for line in lines[3:]:
+        words = line.split()
+        if season_and_club:
+            season_stats["season"] = words[0]
+            if player_profile["nationality"] == words[1]:
+                season_stats["club"] = "N/A"
+            else:
+                season_stats["club"] = " ".join(words[1:])  
+        else:
+            if words[0] in league_abbreviation_to_name:
+                season_stats["league"] = league_abbreviation_to_name[words[0]]
+            else:
+                season_stats["league"] = words[0]
+            season_stats["appearances"] = words[1]
+            # season_stats["minutes"] = words[2]
+            season_stats["goals"] = words[3]
+            season_stats["assists"] = words[4]
+            season_stats["yellow-cards"] = words[5]
+            season_stats["red-cards"] = words[6]
+            # season_stats["shot-per-game"] = words[7]
+            # season_stats["pass-success-percentage"] = words[8]
+            # season_stats["ariel-duels-won"] = words[9]
+            season_stats["man-of-the-matches"] = words[10]
+            season_stats["average-match-rating"] = words[11]                
+        season_and_club = not season_and_club
+        if season_and_club == True:
+            player_stats.append(season_stats.copy())
+            season_stats.clear()
+        
+    return player_stats
 
-def get_player_stats(link):
+def find_leagues_per_club(player_stats):
+    club_to_leagues = defaultdict(set)
+    for season_stats in player_stats:
+        if season_stats["club"] != "N/A":
+            club_to_leagues[season_stats["club"]].add(season_stats["league"])
+    return club_to_leagues
+
+def scrape_player_stats(link):
     options = Options()
     options.headless = False
     options.add_argument(USER_AGENT)
@@ -72,8 +134,9 @@ def get_player_stats(link):
         )
         
         player_profile = parse_player_profile_text(player_profile_scraped.text)
-        print("player_profile: ", player_profile)
-
+        player_stats = parse_player_stats_text(stats_table_scraped.text, player_profile)
+        
+        return player_profile, player_stats
     except Exception as e:
         print("An error occurred:", e)
         driver.quit()
@@ -81,15 +144,13 @@ def get_player_stats(link):
     finally:
         driver.quit()
 
-
-    
-
 links = [
+"https://www.whoscored.com/Players/11119/History/Lionel-Messi",
 "https://www.whoscored.com/Players/50835/History/Neymar",
 "https://www.whoscored.com/Players/2302/History/Xavi",
 "https://www.whoscored.com/Players/5/History/Thierry-Henry",
 "https://www.whoscored.com/Players/29400/History/Robert-Lewandowski",
-"https://www.whoscored.com/Players/315227/History/Erling-Haaland"
+"https://www.whoscored.com/Players/315227/History/Erling-Haaland",
 "https://www.whoscored.com/Players/20874/History/Luka-Modric",
 "https://www.whoscored.com/Players/73084/History/Kevin-De-Bruyne",
 "https://www.whoscored.com/Players/5583/History/Cristiano-Ronaldo",
@@ -110,89 +171,55 @@ links = [
 ]
 
 
-get_player_stats(links[0])
-get_player_stats(links[1])
-get_player_stats(links[2])
-
-
-
-
-
-
-
-
-
-
-# def get_player_names():
-#     # scrape https://www.futbin.com/players to retrieve list of player names
-#     #repTb > tbody > tr:nth-child(2) > td.table-row-text.row > div.d-inline.pt-2.pl-3 > div:nth-child(1) > a
+def update_club_db(club_to_leagues):
+    # takes a dictionary of club name --> list of leagues
+    # for each club in dictionary:
+        # if club does not exist in db, then add club to db along with the leagues it participates in
+        # if club already exists in db, then add any missing leagues to the db
     
-#     player_names = []
-#     return player_names
-
-# def get_player_stats(name):
-#     options = Options()
-#     options.headless = False
-#     options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
-#     service = Service('/Users/dongwoo/Desktop/personal-projects/soccer-player-index/chromedriver-mac-x64/chromedriver',  log_path='/Users/dongwoo/Desktop/personal-projects/soccer-player-index/chromedriver.log')
-#     service.start()
+    # MongoDB connection setup
+    client = pymongo.MongoClient(MONGODB_CONNECTION_STRING)
+    db = client['soccer-player-index']  
+    club_collection = db['club']  
+    for club in club_to_leagues:
+        for league in club_to_leagues[club]:
+            club_collection.update_one(
+                {"name": club},  # Query to find the document
+                {"$addToSet": {"leagues": league}},  # Operation to add league if not exists
+                upsert=True  # Create a new document if one doesn't exist
+            )
+            
+def add_player_to_db(player_profile, clubs):
+    # MongoDB connection setup
+    print("player-profile: ", player_profile)
+    client = pymongo.MongoClient(MONGODB_CONNECTION_STRING)
+    db = client['soccer-player-index']  
+    player_collection = db['player'] 
+    player_document = player_collection.find_one({"name": player_profile["name"]})
+    if player_document == None:
+        player_collection.insert_one({
+            "name": player_profile["name"],
+            "age": player_profile["age"],
+            "nationality": player_profile["nationality"],
+            "positions": player_profile["positions"],
+            "age": player_profile["age"],
+            "age": player_profile["age"],
+            "age": player_profile["age"],
+            "current-club": player_profile["current-club"],
+            "shirt-number": player_profile["shirt-number"],
+            "club-history": clubs
+        })  
     
+# for link in links:
+#     scrape_player_stats(link)
+player_profile, player_stats = scrape_player_stats(links[0])
+club_to_leagues = find_leagues_per_club(player_stats)
+update_club_db(club_to_leagues) 
+clubs = []
+for club in club_to_leagues:
+    clubs.append(club)
+add_player_to_db(player_profile, clubs)
+# scrape_player_stats(links[1])
     
-#     driver = webdriver.Chrome(service=service, options=options)
-#     driver.delete_all_cookies()  # Clear cookies before navigating to the site
-#     driver.command_executor.set_timeout(10)
 
-#     try:
-#         print("herestart")
-#         driver.get('https://www.whoscored.com')
-#         print("here0")
-        
-#         waitForAd = WebDriverWait(driver, 10)
-#         ad_close_button = waitForAd.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[7]/div/div[1]/button')))
-#         ad_close_button.click()
-
-#         search_box = driver.find_element(By.XPATH, '//*[@id="search-box"]')
-#         search_box.send_keys(name)
-#         search_button = driver.find_element(By.XPATH, '//*[@id="search-button"]')
-#         search_button.click()
-
-#         print("here1")
-#         wait = WebDriverWait(driver, 10)
-#         # Click the player link on the search result page
-#         player_link = wait.until(
-#             EC.element_to_be_clickable((By.XPATH, '//*[@id="layout-wrapper"]/div[3]/div[1]/div/table/tbody/tr[2]/td[1]/a'))
-#         )
-#         player_link.click()
-        
-        
-#         print("here2")
-
-
-#         # Click the link on the player profile page to navigate to the stats
-#         stats_link = wait.until(
-#             EC.element_to_be_clickable((By.XPATH, '//*[@id="sub-navigation"]/ul/li[4]/a'))
-#         )
-#         stats_link.click()
-
-#         print("here3")
-
-#         # Ensure the stats table is present
-#         stats_table = wait.until(
-#             EC.presence_of_element_located((By.XPATH, '//*[@id="top-player-stats-summary-grid"]'))
-#         )
-
-#         print("here4")
-
-#         # Now you can extract information from the stats table
-#         # For example, to print out the table text:
-#         print(stats_table.text)
-
-#         # Or to return the table element for further processing outside the function
-#         return stats_table
-
-#     except Exception as e:
-#         print("An error occurred:", e)
-#     finally:
-#         driver.quit()
-# player_stats_table = get_player_stats("Lionel Messi")
 
