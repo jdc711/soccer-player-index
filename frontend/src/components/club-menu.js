e97f5cda541dd9840d5699ad1c1e8b673dbf5c19
Joshua Choe <choejd@usc.edu>
2023-12-01 13:55:08 -0800

    add sliding effect to underline in club-menu

--- frontend/src/components/club-menu.js
+++ frontend/src/components/club-menu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import playerService from '../services/player-service'
 import "./club-menu.css"
 
@@ -7,6 +7,17 @@ const ClubMenu = ({playerId, onSelectedClubIdChange}) => {
     const [error, setError] = useState('');
     const [selectedClubName, setSelectedClubName] = useState("All");
     const [playerProfile, setPlayerProfile] = useState(null);
+    const underlineRef = useRef(null);
+    const allRef = useRef(null);
+
+    const moveUnderline = (element) => {
+        const underline = underlineRef.current;
+        if (underline && element) {
+          const { offsetWidth: width, offsetLeft: left } = element;
+          underline.style.width = `${width}px`;
+          underline.style.left = `${left}px`;
+        }
+      };
     
     useEffect(() => {
       const getPlayerStats = async () => {
@@ -26,6 +37,12 @@ const ClubMenu = ({playerId, onSelectedClubIdChange}) => {
       getPlayerStats();
     }, [playerId]);
     
+    useEffect(() => {
+        if (playerProfile) {
+          // Ensuring the data is loaded and component has rendered
+          moveUnderline(allRef.current);
+        }
+      }, [playerProfile]); // Dependency on playerProfile      
   
     if (loading) {
       return <div className='playerStats loading'>Loading Club Menu...</div>;
@@ -37,13 +54,14 @@ const ClubMenu = ({playerId, onSelectedClubIdChange}) => {
     
     return (
         <div className="clubMenu">
-          <div key={""} onClick={() => {setSelectedClubName("All"); onSelectedClubIdChange(null)}} className={`clubListMenuItem ${selectedClubName === "All" ? 'active' : ''}`}>
+          <div className="underline" ref={underlineRef}></div>
+          <div ref={allRef} key={""} onClick={(e) => {setSelectedClubName("All"); onSelectedClubIdChange(null); moveUnderline(e.currentTarget);}} className={`clubListMenuItem ${selectedClubName === "All" ? 'active' : ''}`}>
             <span >
               All
             </span>
           </div>
             {playerProfile[0]["club_info"] && playerProfile[0]["club_info"].map((club, index) => (
-              <div key={index + '-' + club._id} onClick={() => {setSelectedClubName(club.name); onSelectedClubIdChange(club._id)}} className={`clubListMenuItem ${selectedClubName === club.name ? 'active' : ''}`}>
+              <div key={index + '-' + club._id} onClick={(e) => {setSelectedClubName(club.name); onSelectedClubIdChange(club._id); moveUnderline(e.currentTarget);}} className={`clubListMenuItem ${selectedClubName === club.name ? 'active' : ''}`}>
                 <span>
                   {club.name}
                 </span>
