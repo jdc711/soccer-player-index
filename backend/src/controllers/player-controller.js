799ba83a82f04283d12344ef6a6420f827405caf
Joshua Choe <choejd@usc.edu>
2023-12-09 15:11:47 -0800

    sort player stats list by season on default

--- backend/src/controllers/player-controller.js
+++ backend/src/controllers/player-controller.js
@@ -37,8 +37,12 @@ exports.getPlayerStats = async (req, res) => {
     let playerStats;
     if (!clubId){
       if (sortDirection === ""){
+        console.log("jere")
+        let sort = {};
+        sort["season"] = -1;
         playerStats = await PlayerStats.aggregate([
           { $match: {_player_id: playerId} },
+          { $sort: sort }, 
           { $lookup: { from: 'club', localField: '_club_id', foreignField: '_id', as: 'club_info' } },
           { $lookup: { from: 'league', localField: '_league_id', foreignField: '_id', as: 'league_info' } },
           { $lookup: { from: 'player', localField: '_player_id', foreignField: '_id', as: 'player_info' } },
